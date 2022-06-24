7861e70d574c4a03425afaaedeed3b25ddf6a156
xavsln <19362582+xavsln@users.noreply.github.com>
2022-06-24 18:10:27 +0700

    Create get requests

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const express = require('express');
+const express = require('express'),
+  fs = require('fs');
+
 const app = express();
+
+const topmovies = require('./topmovies.js');
+
+// GET requests
+app.get('/movies', (req, res) => {
+  res.json(topmovies.topMoviesList);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!');
+});
+
+app.listen(8080, () => {
+  console.log('App is running on port 8080');
+});
---
+++ topmovies.js
@@ -0,0 +1,44 @@
+let topMoviesList = [
+  {
+    title: 'Movie 1',
+    director: 'Director 1'
+  },
+  {
+    title: 'Movie 2',
+    director: 'Director 2'
+  },
+  {
+    title: 'Movie 3',
+    director: 'Director 3'
+  },
+  {
+    title: 'Movie 4',
+    director: 'Director 4'
+  },
+  {
+    title: 'Movie 5',
+    director: 'Director 5'
+  },
+  {
+    title: 'Movie 6',
+    director: 'Director 6'
+  },
+  {
+    title: 'Movie 7',
+    director: 'Director 7'
+  },
+  {
+    title: 'Movie 8',
+    director: 'Director 8'
+  },
+  {
+    title: 'Movie 9',
+    director: 'Director 9'
+  },
+  {
+    title: 'Movie 10',
+    director: 'Director 10'
+  }
+];
+
+module.exports.topMoviesList = topMoviesList;
