0cf08b609ae54bba6bb23fdf03e079517ec209a4
xavsln <19362582+xavsln@users.noreply.github.com>
2022-07-26 21:18:41 +0200

    Connect API to mongoDB Atlas

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const Users = Models.User;
 
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
